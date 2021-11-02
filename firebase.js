65906e50deea24330b32eacb2a325168c4394eac
Diego Taveras <diegotaveras2@outlook.com>
2021-11-01 23:51:50 -0400

    change firebase SDK

--- firebase.js
+++ firebase.js
@@ -4,12 +4,12 @@ import { getFirestore, } from 'firebase/firestore/lite';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA1Ms6PoCeS572Bwm-JGIAJeWZjWVtpJ5w",
-  authDomain: "clone-7e710.firebaseapp.com",
-  projectId: "clone-7e710",
-  storageBucket: "clone-7e710.appspot.com",
-  messagingSenderId: "859441695901",
-  appId: "1:859441695901:web:c3b23cde0b9291d7a59874"
+  apiKey: "AIzaSyCWhOvCpUVKNoZdq5AR11ikGHfPjgdcbIo",
+  authDomain: "clone-b9241.firebaseapp.com",
+  projectId: "clone-b9241",
+  storageBucket: "clone-b9241.appspot.com",
+  messagingSenderId: "16078039526",
+  appId: "1:16078039526:web:309261f2186f0e522c8e27"
 };
 
 const app = initializeApp(firebaseConfig);
